faba67c1434ddfc192c6489b83170a54adbe7149
erspicu_brox <erspicu_brox@yahoo.com.tw>
2016-12-27 10:20:54 +0800

    AprSpeedBenchmark
    FIRST

---
+++ AprSpeedBenchmark/BenchMarkUI.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GBA_effect
+{
+    partial class BenchMarkUI
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BenchMarkUI));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(13, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(512, 512);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.benchmark_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 561);
+            this.progressBar1.MarqueeAnimationSpeed = 10;
+            this.progressBar1.Maximum = 68904000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(511, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 598);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "平均時速 : ";
+            // 
+            // BenchMarkUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 636);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "BenchMarkUI";
+            this.Text = "SpeedBenchmark";
+            this.Shown += new System.EventHandler(this.BenchMarkUI_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AprSpeedBenchmark/BenchMarkUI.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using NativeWIN32API;
+namespace GBA_effect
+{
+    unsafe public partial class BenchMarkUI : Form
+    {
+        static uint* ScreenBg, Sprite, result;
+
+        public BenchMarkUI()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void BenchMarkUI_Shown(object sender, EventArgs e)
+        {
+            for (int y = 0; y < 512; y++)
+                for (int x = 0; x < 512; x++)
+                    result[x + y * 512] = ScreenBg[x + (y << 9)];
+
+            for (int y = 0; y < 256; y++)
+                for (int x = 0; x < 256; x++)
+                {
+                    uint color = Sprite[x + y * 256];
+                    if ((color & 0xff000000) != 0) result[(x + 128) + ((y + 128) << 9)] = color;
+                }
+            NativeGDI.DrawImageHighSpeedtoDevice();
+        }
+
+        void init()
+        {
+            {
+                Bitmap bg = new Bitmap(Application.StartupPath + "/bg.png");
+                ScreenBg = (uint*)Marshal.AllocHGlobal(sizeof(uint) * 512 * 512);
+                BitmapData srcData = bg.LockBits(new Rectangle(Point.Empty, bg.Size), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                for (int y = 0; y < 512; y++)
+                    for (int x = 0; x < 512; x++)
+                        ScreenBg[x + y * 512] = ((uint*)srcData.Scan0.ToPointer())[x + y * 512];
+                bg.UnlockBits(srcData);
+            }
+
+            {
+                Bitmap sp = new Bitmap(Application.StartupPath + "/wheel.png");
+                Sprite = (uint*)Marshal.AllocHGlobal(sizeof(uint) * 256 * 256);
+                BitmapData srcData = sp.LockBits(new Rectangle(Point.Empty, sp.Size), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                for (int y = 0; y < 256; y++)
+                    for (int x = 0; x < 256; x++)
+                        Sprite[x + y * 256] = ((uint*)srcData.Scan0.ToPointer())[x + y * 256];
+                sp.UnlockBits(srcData);
+            }
+            result = (uint*)Marshal.AllocHGlobal(sizeof(uint) * 512 * 512);
+            NativeGDI.initHighSpeed(panel1.CreateGraphics(), 512, 512, result, 0, 0);
+
+        }
+
+        bool runnging = false;
+
+        private void benchmark_Click(object sender, EventArgs e)
+        {
+            if (runnging)
+            {
+                MessageBox.Show("testing....");
+                Console.WriteLine("running...wait!");
+                return;
+            }
+
+            progressBar1.Value = 0;
+            label3.Text = "平均時速 : ";
+
+            new Thread(() =>
+            {
+                runnging = true;
+                run();
+                runnging = false;
+            }).Start();
+        }
+
+        void run()
+        {
+            Stopwatch st = new Stopwatch();
+            st.Restart();
+
+            const int ref_loc_x = 128 ,ref_loc_y = 128;
+
+            double angle = 0 , _angle , w = 0;
+            int counts = 36000 , dist = 0;
+
+            for (int i = 0; i < counts; i++)
+            {
+
+                _angle = angle * 0.01745329252;
+
+                double cos = Math.Cos(_angle);
+                double sin = Math.Sin(_angle);
+
+                for (int y = 0; y < 512; y++)
+                    for (int x = 0; x < 512; x++)
+                        result[x + (y << 9)] = ScreenBg[x + (y << 9)];
+
+                Parallel.For(0, (256), y =>
+                {
+                    for (int x = 0; x < 256; x++)
+                    {
+
+                        uint color = Sprite[x + (y << 8)];
+
+                        int index_dst = ((int)((x - ref_loc_x) * cos + (y - ref_loc_y) * sin + ref_loc_x + 128)) +
+                        (((int)(-1.0 * (x - ref_loc_x) * sin + (y - ref_loc_y) * cos + ref_loc_y + 128)) << 9);
+
+                        if ((color & 0xff000000) != 0) result[index_dst] = result[index_dst - 1] = color;
+
+                    }
+                });
+                NativeGDI.DrawImageHighSpeedtoDevice();
+
+                angle += 6;
+                dist += 1914;
+                w += 0.0166666666666667;
+
+                if (angle >= 360)
+                {
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        progressBar1.Value = dist;
+                    }));
+                    angle -= 360;
+                }
+            }
+
+            st.Stop();
+
+            int ws = (int)(w / ((double)st.ElapsedMilliseconds / (1000.0 * 60)));
+            int speed = (int)((ws * 60 * 1.914) / 1000.0);
+
+            Console.WriteLine("Cost : " + st.ElapsedMilliseconds + " ms");
+            Console.WriteLine("RPM : " + ws);
+            Console.WriteLine("Avg Speed : " + speed + " Km/H");
+            Console.WriteLine("dist : " + dist / 1000.0 + " m");
+
+            this.Invoke(new MethodInvoker(() =>
+            {
+                label3.Text = "平均時速 : " + speed + "km/h";
+            }));
+
+        }
+    }
+}
